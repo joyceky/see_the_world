851c43b93692aa817dfa30da1106d74bfe1ca2a2
Kyle Joyce <kyyjoyce@gmail.com>
2017-06-13 12:18:32 -0400

    fix footer positioning issue, add more detailed comments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # urbn_code_challenge
-An exploratory photo map with color palette generator.
+An exploratory photo map. 
+This project was created by Kyle Joyce using the Google Maps API and the Flickr API.
--- js/photoMap.js
+++ js/photoMap.js
@@ -1,21 +1,25 @@
 "use strict";
 
+/* 
+    Global variables for accessing Flickr API, lat, long, map, and UI elements
+    Lat and long initially hardcoded to Philly
+*/
 const FLICKR_API_KEY = "97690e43438d1a5f0402a1ffa7c557fd";
-const FLICKR_SECRET = "b644c1c1861cb8f7";
-const GMAPS_API_KEY = "AIzaSyA6t4HGkRl-ZJxvpSHleNmFDlQyem-Eh5I";
 
-const tags = ["nature", "joy", "world", "landscape", "wander", "beautiful", "outdoor", "green", "view", "light", "lines", "nature", "rural", "vacation", "love", "pride", "travel", "trees", "island", "life", "explore", "adventure", "water", "fashion", "style", "beautiful", "photography", "fish", "light", "inspiring", "food", "sunset", "sky", "wildlife", "snow", "fashion", "fields", "hike", "happiness", "animals", "kittens", "city", "forest", "beach", "mountain", "sunshine", "rain", "puppy", "puppies"];
-
-var lat = 39.9526;
+var lat = 39.9526; 
 var long = -75.1652;
 var map;
 
 var imageDetail = $("#imageDetail");
 var noPhotosAlert = $("#noPhotosAlert");
 
-initMap();
+$(function() {
+    // When the document is ready, call the initMap() function
+    initMap();
+});
 
 function initMap() {
+    // initMap() creates a new Google map with all user controls 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: lat,
@@ -31,19 +35,27 @@ function initMap() {
         fullscreenControl: true
     });
 
+    // Then the center of the map is set with a marker on Philadelphia
     var philly = new google.maps.LatLng(lat, long);
 
-    var marker = new google.maps.Marker({
+    new google.maps.Marker({
         position: philly,
         map: map
     });
 
+    // The reverse-geocoder and infowindow the display geocode results are initialized
     var geocoder = new google.maps.Geocoder;
     var infowindow = new google.maps.InfoWindow;
     
-    // geocodeLatLng(geocoder, map, infowindow, lat, long);
-    getImages(lat, long)
+    // The getImages() function is then called with the initial lat, long (Philadelphia)
+    getImages(lat, long);
 
+    /* 
+        A click event listener is applied to the map
+        When the map is clicked, the lat, long is grabbed from the click event
+        The center of the map is set the the lat, long 
+        A marker is placed at the lat, long 
+    */
     map.addListener('click', function(event) {
         $('#map').animate({
             height: '50vh'
@@ -51,35 +63,48 @@ function initMap() {
                 initMap();
                 map.setCenter(event.latLng);
                 var coordinates = event.latLng;
-                lat = coordinates.lat();
-                long = coordinates.lng();
+                lat = (coordinates.lat()).toFixed(6);
+                long = (coordinates.lng()).toFixed(6);
 
-                console.log(lat, long);
+                // console.log("Lat: ", lat, "Long: ", long);
 
-                var marker = new google.maps.Marker({
+                new google.maps.Marker({
                     position: coordinates,
                     map: map
                 });
 
+                // Attempt to get reverse-geocode information for current lat, long
                 geocodeLatLng(geocoder, map, infowindow, lat, long);
+                // Retrieve images for current location
                 getImages(lat, long);
         });
     });
 }
 
 function getImages(lat, long) {
+    // In case the noPhotosAlert div is shown, fade it out and empty the content
     noPhotosAlert.fadeOut();
     noPhotosAlert.empty();
 
+    /* 
+        A call is made to the Flickr API with the lat, long to request photos tagged in that location
+        Note: API call fails initially based on latitude but does return correct photo set
+    */
     $.ajax({
         url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&lat=" + lat + "&lon=" + long + "&per_page=100&format=json&jsoncallback=?",
         type: "GET",
         dataType: 'jsonp',
         success: function (data) {
-            console.log("Images successfully retrieved", data);
+            console.log("DATA: ", data);
+            if ( data.stat == "fail" ) {
+                console.log("Flickr latitude issue");
+            }
+            else if (data.photos.total < 1) {
+                console.log("Location images not retrieved: ", data)
+                // If there are no photos, display stock photos from the array of tags below and alert the user
+                const tags = ["nature", "joy", "world", "landscape", "wander", "beautiful", "outdoor", "green", "view", "light", "lines", "nature", "rural", "vacation", "love", "pride", "travel", "trees", "island", "life", "explore", "adventure", "water", "fashion", "style", "beautiful", "photography", "fish", "light", "inspiring", "food", "sunset", "sky", "wildlife", "snow", "fashion", "fields", "hike", "happiness", "animals", "kittens", "city", "forest", "beach", "mountain", "sunshine", "rain", "puppy", "puppies"];
 
-            if (data.photos.photo.length == 0) {
-                // If there are no photos, display stock photos from a variety of tags and alert user
+                // A backup Flickr API call to provide photos with one of the tags above
                 $.ajax({
                     url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_API_KEY + "&tags=" + tags[Math.floor(Math.random() * tags.length+1) + 1]+ "&per_page=100&format=json&jsoncallback=?",
                     type: "GET",
@@ -87,18 +112,21 @@ function getImages(lat, long) {
                     success: function (data) {
                         console.log("Images successfully retrieved: ", data);
                         
-                        var htmlString = '<p>No photos tagged in this location. Keep clicking to see the world!</p>';					
-                        $(htmlString).appendTo(noPhotosAlert);
-                        noPhotosAlert.fadeIn();
+                        // Show the user a div to let them know that there are no photos in their selected area
+                        showNoPhotosAlert();
 
+                        // Display the retrieved images
                         displayImages(data);
                     },
                     fail: function (err) {
                         console.log("Error retrieving images: " + err.status);
                     }
                 });
-            }
+              }
             else {
+                console.log("Images successfully retrieved", data);
+                
+                // Display the retrieved images
                 displayImages(data);
             }
         },
@@ -108,6 +136,13 @@ function getImages(lat, long) {
     });
 }
 
+/*
+    geocodeLatLng() takes the initialized geocoder, map, and infowindow plus the lat, long
+    An attempt is made to gather the location information from the lat, long 
+    If location results are found, the location is zoomed in on and a marker is placed
+    The infowindow will display and show the location place name
+    If no results are found,a console error will display
+*/
 function geocodeLatLng(geocoder, map, infowindow, lat, long) {
     var latlng = {lat: parseFloat(lat), lng: parseFloat(long)};
     geocoder.geocode({'location': latlng}, function(results, status) {
@@ -143,28 +178,38 @@ function displayImages(data){
         
         // Create the url for the current photo 
         var photoURL = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + '_' + photo.secret + '_m.jpg';		
-        // console.log(photoURL, "PHOTO URL");
 
         // Create the resulting img tag for each photo 
-        var htmlString = '<img class="image" id="' + photoURL + '" src="' + photoURL + '">';					
-        // console.log(htmlString, "HTML STRING");
+        var title = photo.title || "Description not provided";
+        var htmlString = '<img class="image" title="' + title + '" alt="' + title + '" id="' + photoURL + '" src="' + photoURL + '">';					
 
+        // Append the img tag to the photos div and then fade it in
         $(htmlString).appendTo("#photos");
     });	
     
     $('#photos').fadeIn(1000);	
-
-	
   } 
 
-function showDetail(URL) {
-    imageDetail.empty();
-    var imageString = '<img class="image" src="' + URL + '">';					
+// Shows the user a div to let them know that there are no photos in their selected area
+function showNoPhotosAlert() {
+    var htmlString = '<p>No photos tagged in this location. Keep clicking to see the world!</p>';					
+    $(htmlString).appendTo(noPhotosAlert);
+    noPhotosAlert.fadeIn();
+}  
 
+// Dims the screen background and pops up the image clicked by the user with the title provided by Flickr users
+function showDetail(URL, title) {
+    imageDetail.empty();
+    var titleString = '<p>' + title + '</p>';
+    var imageString = '<img class="image" src="' + URL + '">';	
+
+    $(titleString).appendTo("#imageDetail");
     $(imageString).appendTo("#imageDetail");
+
     imageDetail.show();
 }
 
+// If the imageDetail div is visible, it is emptied and hidden on click
 function toggleDetail() {
     if (imageDetail.is(":visible")) {
         imageDetail.empty();
